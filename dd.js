b07ff76cbc21a123ae3c4c3683861f61d04240d5
Jubaid-Islam <jubaid20062@gmail.com>
2026-09-02 23:30:03 +0600

    fix service account key

---
+++ dd.js
@@ -0,0 +1,4 @@
+import fs from 'fs';
+const key = fs.readFileSync('./serviceAccountKey.json')
+const base64 = Buffer.from(key, 'utf8').toString('base64')
+console.log(base64);
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,11 +1,14 @@
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
-import serviceAccount from '../../serviceAccountKey.json' with { type: 'json' };
 
+const serviceAccount = JSON.parse(
+    Buffer.from(process.env.SERVICE_ACCOUNT_KEY, 'base64').toString('utf-8')
+);
 
 const app = initializeApp({
-  credential: cert(serviceAccount)
+    credential: cert(serviceAccount),
 });
 
 export const adminAuth = getAuth(app);
+
 export default app;
